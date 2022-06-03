276c7d40cda2b00cae5ecf16e5604be852c165ec
Mohamed Hossein Talha <mhtalha@dom.tti>
2022-06-03 18:04:42 +0100

    activit1.2

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<div class="card text-center" >
+  
+  <div class="card-body">
+    <h5 class="card-title">Talan Academy: PHP program</h5>
+    <p class="card-text"><p>All rights reserved © <?php echo date("Y")?></p></p>
+    <a href="mailto:mohamed-hossein.talha@talan.com" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</body>
+</html>
+<footer>
+  
+</footer>
